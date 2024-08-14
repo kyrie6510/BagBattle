dd9fa5deb9a509daceab9c241eb9ae780ad70064
liurushi <1936298733@qq.com>
2024-08-14 17:30:01 +0800

    Fix atkSystem

--- Project/Assets/Game/Buff/BuffTimingAtkSystem.cs
+++ Project/Assets/Game/Buff/BuffTimingAtkSystem.cs
@@ -13,27 +13,31 @@ namespace Game
         {
             var config = ConfigManager.Instance.GetTimConfig(buff.timingConfigId.Value);
 
-
             var attachEntity = Contexts.sharedInstance.game.GetEntityWithLocalId(buff.attachId.Value);
-
+            //对手未命中
+            if (attachEntity.timingTypeAtk.Value != buff.timingTypeAtk.Value)
+            {
+                return;
+            }
+            
+           
+            //攻击命中
             if (buff.buffTimingListenTarget.ListTargetType == (int) ListenTarget.Self)
             {
-                //攻击命中
-                if (attachEntity.timingTypeAtk.Value == buff.timingTypeAtk.Value)
+                
+
+                //触发效果
+                foreach (var effectId in buff.buffEffectId.Value)
                 {
-                    //触发效果
-                    foreach (var effectId in buff.buffEffectId.Value)
-                    {
-                        var effectConfig = ConfigManager.Instance.GetEffectConfig(effectId);
-                        EventManager.Instance.TriggerEvent(new BattleLog(attachEntity.actorId.Value,
-                            $"actor:{attachEntity.actorId.Value} {effectConfig.Name}"));
-                        EffectManager.Instance.CreatEffect(effectId, attachEntity.actorId.Value, attachEntity.localId.value,
-                            buff.localId.value);
-                    }
+                    var effectConfig = ConfigManager.Instance.GetEffectConfig(effectId);
+                    EventManager.Instance.TriggerEvent(new BattleLog(attachEntity.actorId.Value, $"actor:{attachEntity.actorId.Value} {effectConfig.Name}"));
+                    EffectManager.Instance.CreatEffect(effectId, attachEntity.actorId.Value, attachEntity.localId.value, buff.localId.value);
                 }
+                
             }
 
-            //对手未命中
+         
+            //未命中    
             else if (buff.buffTimingListenTarget.ListTargetType == (int) ListenTarget.OtherActorWeapon)
             {
                 var other = GetOtherActor(buff.attachActorId.Value);
